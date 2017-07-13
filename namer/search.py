12053c56383aab1e2b9e7629563c40fb0f6d040d
Jeremy Ho <jujaga@gmail.com>
2017-07-13 12:42:07 -0700

    Add rudimentary trie search
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- namer/search.py
+++ namer/search.py
@@ -1,26 +1,62 @@
+import bisect
+import csv
+import os
+import re
+
 from pytrie import SortedStringTrie as Trie
 
 
-def search():
-    trie = Trie()
-    trie.update({'an': 0})
-    trie.update({'ant': 1})
-    trie.update({'all': 2})
-    trie.update({'allot': 3})
-    trie.update({'alloy': 4})
-    trie.update({'aloe': 5})
-    trie.update({'are': 6})
-    trie.update({'be': 7})
+class Search(object):
+    def __init__(self):
+        """Initialization"""
+        self.search_trie = None
 
-    print(trie.keys(prefix='al'))
-    print(trie.items(prefix='an'))
-    print(trie.longest_prefix('antonym'))
-    print(trie.longest_prefix_item('allstar'))
-    print(trie.longest_prefix_value('area', default='N/A'))
-    print(trie.longest_prefix_value('alsa', default=-1))
-    print(list(trie.iter_prefixes('allotment')))
-    print(list(trie.iter_prefix_items('antonym')))
+        self._init_searchtrie()
 
+    def _init_searchtrie(self):
+        """Builds the searchtrie from input data into memory"""
+        file_path = os.path.join(os.path.dirname(__file__), '..', 'files',
+                                 'data-100k.csv')
+        if os.path.isfile(file_path) and self.search_trie is None:
+            self.search_trie = Trie()
+            self.load_data(file_path)
+
+    def load_data(self, file_path):
+        """
+        Temporary Proof of concept function
+        Loads information from CSV file
+        Parses and splits CORP_NME for search_trie
+        """
+        with open(file_path) as file:
+            reader = csv.DictReader(file, delimiter=';')
+            try:
+                for row in reader:
+                    clean_name = re.sub(r'[^a-zA-Z\d\s]', '', row['CORP_NME'])
+                    for element in clean_name.split():
+                        if element is not None:
+                            if element not in self.search_trie:
+                                self.search_trie[element] = set()
+                            self.search_trie[element].add(row['CORP_NUM'])
+            except UnicodeDecodeError:
+                print('Unexpected input at line ' + reader.line_num)
+
+    def search(self, prefix):
+        """Searches and returns a list of values which match the key"""
+        try:
+            print(self.search_trie[prefix])
+            print(prefix in self.search_trie)
+            return self.search_trie.itervalues(prefix)
+
+        except KeyError:
+            return None
 
 if __name__ == "__main__":
-    search()
+    from timeit import default_timer as timer
+
+    engine = Search()
+    start = timer()
+    results = engine.search('ESTATE')
+    end = timer()
+    for result in results:
+        print(result)
+    print('Search time: ' + str(end - start))
